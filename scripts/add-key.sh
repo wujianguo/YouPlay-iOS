3751988dc93e65e9fc623d50fef963b592fbcd7b
wujianguo <lsjustin89@gmail.com>
2016-01-13 18:53:41 +0800

    ci

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -2,6 +2,6 @@
 security create-keychain -p travis ios-build.keychain
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $ENCRYPTION_SECRET -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./scripts/profile/wujianguoAdHoc.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
