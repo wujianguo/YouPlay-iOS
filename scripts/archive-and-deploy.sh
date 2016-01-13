b0fedcaee5dd444efc0525b4965531e2714f7990
wujianguo <lsjustin89@gmail.com>
2016-01-13 18:33:27 +0800

    ci

--- scripts/archive-and-deploy.sh
+++ scripts/archive-and-deploy.sh
@@ -1,1 +1,3 @@
 #!/bin/sh
+xcodebuild archive -project YouPlay-iOS.xcodeproj -scheme YouPlay-iOS -archivePath YouPlay
+xcodebuild -exportArchive -archivePath YouPlay.xcarchive -exportPath YouPlay -exportFormat ipa
