b5556cd1b49a128f895fc45c7f896c1283533fea
wujianguo <lsjustin89@gmail.com>
2016-01-14 11:20:48 +0800

    ci

--- scripts/archive-and-deploy.sh
+++ scripts/archive-and-deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-xcodebuild archive -project $APP_NAME.xcodeproj -scheme $APP_NAME -archivePath $APP_NAME
-xcodebuild -exportArchive -archivePath $APP_NAME.xcarchive -exportPath $APP_NAME -exportFormat ipa
-curl -F "file=@$APP_NAME.ipa" \
+# - xctool -project $APP_NAME.xcodeproj -scheme $APP_NAME -sdk iphonesimulator ONLY_ACTIVE_ARCH=YES
+
+PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
+OUTPUTDIR="$PWD/build/Release-iphoneos"
+
+xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+
+curl -F "file=@$OUTPUTDIR/$APP_NAME.ipa" \
      -F "uKey=$PGYER_USER_KEY" \
      -F "_api_key=$PGYER_API_KEY" \
      http://www.pgyer.com/apiv1/app/upload
