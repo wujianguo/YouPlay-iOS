398f949d2d9485060eaa29da6d94126e4b08394f
wujianguo <lsjustin89@gmail.com>
2016-01-14 12:13:33 +0800

    ci

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -2,7 +2,7 @@
 security create-keychain -p travis ios-build.keychain
 security default-keychain -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
-security set-keychain-settings -t 3600 -u iso-build.keychain
+security set-keychain-settings -t 3600 -u ios-build.keychain
 security import apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import sanliaoDistribution.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import sanliaoDistribution.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
