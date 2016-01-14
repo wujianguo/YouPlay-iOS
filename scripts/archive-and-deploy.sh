48f94b393fac6c4e7cebcd74e3384b288fbd7406
wujianguo <lsjustin89@gmail.com>
2016-01-14 14:39:51 +0800

    ci

--- scripts/archive-and-deploy.sh
+++ scripts/archive-and-deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # - xctool -project $APP_NAME.xcodeproj -scheme $APP_NAME -sdk iphonesimulator ONLY_ACTIVE_ARCH=YES
+# - xctool -project $APP_NAME.xcodeproj -scheme $APP_NAME -sdk iphoneos -configuration Release  OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO CODE_SIGNING_REQUIRED=NO
+
 # - xctool -project $APP_NAME.xcodeproj -scheme $APP_NAME -sdk iphoneos -configuration Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO CODE_SIGN_IDENTITY=$DEVELOPR_NAME PROVISIONING_PROFILE=$PROVISIONING_PROFILE_UUID
 
-PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
+PROVISIONING_PROFILE="~/Library/MobileDevice/Provisioning Profiles/wujianguoAdHoc.mobileprovision"
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
-xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPR_NAME_CO" -embed "$PROVISIONING_PROFILE"
 
 curl -F "file=@$OUTPUTDIR/$APP_NAME.ipa" \
      -F "uKey=$PGYER_USER_KEY" \
