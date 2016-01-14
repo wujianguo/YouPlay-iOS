74a92e23a7f66dbcbb3da2a5f5542724cdb5149f
wujianguo <lsjustin89@gmail.com>
2016-01-14 11:02:55 +0800

    ci

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -3,8 +3,8 @@ security create-keychain -p travis ios-build.keychain
 security default-keychain -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 security set-keychain-settings -t 3600 -u iso-build.keychain
-# security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-# security import ./scripts/certs/sanliaoDistribution.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./sanliaoDistribution.p12 -k ~/Library/Keychains/ios-build.keychain -P sanliao -T /usr/bin/codesign
+security import apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import sanliaoDistribution.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import sanliaoDistribution.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./wujianguoAdHoc.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
--- scripts/archive-and-deploy.sh
+++ scripts/archive-and-deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-xcodebuild archive -project YouPlay-iOS.xcodeproj -scheme YouPlay-iOS -archivePath YouPlay
-xcodebuild -exportArchive -archivePath YouPlay.xcarchive -exportPath YouPlay -exportFormat ipa
+xcodebuild archive -project $APP_NAME.xcodeproj -scheme $APP_NAME -archivePath $APP_NAME
+xcodebuild -exportArchive -archivePath $APP_NAME.xcarchive -exportPath $APP_NAME -exportFormat ipa
+curl -F "file=@$APP_NAME.ipa" \
+     -F "uKey=$PGYER_USER_KEY" \
+     -F "_api_key=$PGYER_API_KEY" \
+     http://www.pgyer.com/apiv1/app/upload
